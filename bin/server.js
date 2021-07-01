d36482e555fe637a6c4fba4d9d2adbb78430db66
Roman Didukh <didukhroma@gmail.com>
2021-07-01 20:36:31 +0300

    add pre-commit husky

--- bin/server.js
+++ bin/server.js
@@ -5,11 +5,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running. Use our API on port: ${PORT}`);
 });
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa;
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa;
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa;
-bbbbbbbbbbbbbbbbb;
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa;
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa;
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa;
-bbbbbbbbbbbbbbbbb;
