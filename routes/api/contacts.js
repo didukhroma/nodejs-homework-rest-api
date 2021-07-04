1a757d31112bce0684f5e0f75595b7e112fa741b
Roman Didukh <didukhroma@gmail.com>
2021-07-04 17:27:02 +0300

    bug fix with validation

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const contactsControllers = require('../../src/controllers');
 const router = express.Router();
+const {
+  validateCreateContact,
+  validateUpdateContact,
+} = require('../../src/validation/contacts');
 
 router
   .get('/', contactsControllers.getAll)
   .get('/:contactId', contactsControllers.getById)
-  .post('/', contactsControllers.addNewContact)
+  .post('/', validateCreateContact, contactsControllers.addNewContact)
   .delete('/:contactId', contactsControllers.remove)
-  .put('/:contactId', contactsControllers.update);
+  .put('/:contactId', validateUpdateContact, contactsControllers.update);
 
 module.exports = router;
--- src/controllers/contacts/addNewContact.js
+++ src/controllers/contacts/addNewContact.js
@@ -1,26 +1,7 @@
-const Joi = require('joi');
 const { HttpCode } = require('../../helpers/constants');
 const { addContact } = require('../../../model');
 
-const contactSchema = Joi.object({
-  name: Joi.string().alphanum().min(3).max(30).required(),
-  email: Joi.string()
-    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
-    .required(),
-  phone: Joi.string().min(10).required(),
-});
-
 const addNewContact = async (req, res, next) => {
-  const { error } = contactSchema.validate(req.body);
-  if (error) {
-    res.status(400).json({
-      status: 'error',
-      code: 400,
-      message: error.message,
-    });
-    return;
-  }
-
   try {
     const newContact = await addContact(req.body);
     if (!newContact) {
